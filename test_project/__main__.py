87d04f7df0f69c4f2e3bfd263fc8c5906df7b16e
oliver <oliver@wayve.ai>
2023-08-16 14:25:24 +0100

    Fixed an issue with the patches BUILD.bazel

--- test_project/__main__.py
+++ test_project/__main__.py
@@ -1,9 +1,7 @@
 if __name__ == "__main__":
     from main import main
 
-    import triton
     import torch
-    import py3nvml
 
     tensor = torch.ones((1, 2, 3))
     print(f"testing torch {tensor=}")
