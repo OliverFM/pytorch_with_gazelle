b9d1c97863b75817b9b37b6496e1e8ef9b3654cf
oliver <oliver@wayve.ai>
2023-08-22 20:21:39 +0100

    Latest progress on pyo3

---
+++ pyo3_example/src/example_lib.rs
@@ -0,0 +1,26 @@
+use pyo3::prelude::Python;
+use pyo3::prelude::*;
+use pyo3::types::PyModule;
+use pyo3::PyResult;
+
+// /// This module is implemented in Rust.
+#[pymodule]
+fn my_ext(_py: Python, m: &PyModule) -> PyResult<()> {
+    #[pyfn(m, "sum_as_string")]
+    fn sum_as_string_py(_py: Python, a: i64, b: i64) -> PyResult<String> {
+        let out = sum_as_string(a, b);
+        Ok(out)
+    }
+
+    Ok(())
+}
+
+// logic implemented as a normal Rust function
+fn sum_as_string(a: i64, b: i64) -> String {
+    format!("{}", a + b)
+}
+
+// #[pyfunction]
+// fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+//     Ok((a + b).to_string())
+// }
---
+++ pyo3_example/src/lib.rs
@@ -0,0 +1,4 @@
+#[macro_use]
+extern crate pyo3;
+
+pub mod example_lib;
