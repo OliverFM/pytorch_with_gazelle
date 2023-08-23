9b9aa3215687d3a3947e8b8da9b0c388ac306fa9
oliver <oliver@wayve.ai>
2023-08-23 20:00:09 +0100

    Got PyO3 working in Bazel

--- pyo3_example/example.py
+++ pyo3_example/example.py
@@ -1,5 +1,13 @@
-from string_sum import sum_as_string
-
+# import ipdb; ipdb.set_trace()
+import sys
+import os
+# print(sys.version)
+# print(sys.path)
+# print(os.listdir('pyo3_example'))
+# sys.path.append(os.getcwd() + "/pyo3_example")
+# sys.path.append(os.getcwd() + "/pyo3_example/example_lib.so")
+# print(sys.path)
+from  example_lib import sum_as_string
 
 def main():
     result = sum_as_string(1,4)
